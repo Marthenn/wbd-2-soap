611b92822a6b3e70b8b35418db2648f6a92423e5
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-10 13:53:49 +0700

    feat (logging): created logging model

---
+++ src/main/java/webwbd/model/Logging.java
@@ -0,0 +1,37 @@
+package webwbd.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+public class Logging {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    @Getter
+    private int id;
+
+    @Lob
+    @Column(name = "description")
+    @Getter
+    @Setter
+    private String description;
+
+    @Column(name = "ip_address", nullable = false)
+    @Getter
+    @Setter
+    private String ipAddress;
+
+    @Column(name = "endpoint", nullable = false)
+    @Getter
+    @Setter
+    private String endpoint;
+
+    @Column(name = "request_time", nullable = false)
+    @Getter
+    @Setter
+    private Date requestTime;
+}
