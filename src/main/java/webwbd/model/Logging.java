1c735a4b7bcbd02557ffd3dce981fd32bb0685af
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-10 14:14:36 +0700

    chore(Logging): move annotation of getter and add length to annotation of ip

--- src/main/java/webwbd/model/Logging.java
+++ src/main/java/webwbd/model/Logging.java
@@ -6,32 +6,28 @@ import lombok.Setter;
 import java.util.Date;
 import javax.persistence.*;
 
+@Getter
 @Entity
 public class Logging {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    @Getter
     private int id;
 
     @Lob
     @Column(name = "description")
-    @Getter
     @Setter
     private String description;
 
-    @Column(name = "ip_address", nullable = false)
-    @Getter
+    @Column(name = "ip_address", nullable = false, length = 16)
     @Setter
     private String ipAddress;
 
     @Column(name = "endpoint", nullable = false)
-    @Getter
     @Setter
     private String endpoint;
 
     @Column(name = "request_time", nullable = false)
-    @Getter
     @Setter
     private Date requestTime;
 
