bfcb93db00cf60984f5369f916daffbd2bdd3363
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-10 14:36:29 +0700

    feat (request): create request model

---
+++ src/main/java/webwbd/model/Request.java
@@ -0,0 +1,33 @@
+package webwbd.model;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+public class Request {
+    @Id
+    @GeneratedValue
+    @Column(name = "request_id", nullable = false)
+    private int requestId;
+
+    @Column(name="username", nullable = false, length = 256, unique = true)
+    @Setter
+    private String username;
+
+    @Column(name = "email", nullable = false, length = 256, unique = true)
+    @Setter
+    private String email;
+
+    @Column(name = "date", nullable = false)
+    @Setter
+    private Date date;
+
+    @Column(name = "proof_directory", nullable = false)
+    @Setter
+    private String proofDirectory;
+}
