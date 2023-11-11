7c53bd087b5354cd26d48c79928d4171108f2c11
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-11 23:49:11 +0700

    feat (service): create placeholder service to test docker

--- src/main/java/webwbd/service/Service.java
+++ src/main/java/webwbd/service/Service.java
@@ -4,5 +4,13 @@ import javax.jws.*;
 
 @WebService
 public class Service {
+    @WebMethod
+    public String hello(String name) {
+        return "Hello " + name;
+    }
 
+    @WebMethod
+    public String hello(){
+        return "Hello World";
+    }
 }
