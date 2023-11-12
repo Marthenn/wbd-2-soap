2f42ce49daa641778080eb3c49d347d41e438fee
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-12 13:24:42 +0700

    chore(request): add two stub for approve and decline request

--- src/main/java/webwbd/repository/RequestRepository.java
+++ src/main/java/webwbd/repository/RequestRepository.java
@@ -8,7 +8,7 @@ import webwbd.util.HibernateUtil;
 import java.util.Date;
 
 public class RequestRepository {
-    public Boolean createRequest(int id, String username, String email, String proofDirectory) {
+    public Boolean createRequest(String username, String email, String proofDirectory) {
         try {
             Request request = new Request();
             request.setUsername(username);
@@ -23,6 +23,34 @@ public class RequestRepository {
             session.save(request);
             session.getTransaction().commit();
 
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Boolean approveRequest(int id) {
+        try {
+            // TODO: implement approveRequest
+            /*
+            * 1. Request id sudah ada (dari parameter)
+            * 2. Set status request menjadi Approved
+            * 3. Set description jadi "Request approved by admin at {date}"
+            * */
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Boolean declineRequest(int id) {
+        try {
+            // TODO: implement declineRequest
+            /*
+             * 1. Request id sudah ada (dari parameter)
+             * 2. Set status request menjadi Declined
+             * 3. Set description jadi "Request declined by admin at {date}. Cause: {cause}"
+             * */
             return true;
         } catch (Exception e) {
             return false;
